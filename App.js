57f5d13ea6d4ca1e40adc233e949c4cf4bdf4da0
Cinthya <cinthyaredondo12@gmail.com>
2024-09-05 18:01:12 +0200

    I added a new app

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, Text, View, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import Header from "./src/components/Header.jsx";
+import Timer from "./src/components/Timer.jsx";
+
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" || "SHORT" || "BREAK");
+  const {isActive, setIsActive}=useState(false)
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>hoola</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <View
+        style={{
+          flex:1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 30,
+        }}
+      >
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer time={time} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={{color:"white", fontWeight:"bold"}}>{isActive?"STOP":"START"}</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: { fontSize: 32, fontWeight: "bold" },
+  button:{
+    backgroundColor:"#333333",
+    padding:15,
+    marginTop:15,
+    borderRadius:15,
+    alignItems:"center"
+  }
 });
