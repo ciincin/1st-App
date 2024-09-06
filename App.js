89ba6851d708dfd740587e053e069bcd19d4ffac
Cinthya <cinthyaredondo12@gmail.com>
2024-09-06 12:40:03 +0200

    I finish my first app - POMODORO

--- App.js
+++ App.js
@@ -1,17 +1,58 @@
-import { StyleSheet, SafeAreaView, Platform, Text, View, TouchableOpacity } from "react-native";
-import { useState } from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useState } from "react";
 import Header from "./src/components/Header.jsx";
 import Timer from "./src/components/Timer.jsx";
+import { Audio } from "expo-av";
 
 const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
   const [isWorking, setIsWorking] = useState(false);
-  const [time, setTime] = useState(25 * 60);
-  const [currentTime, setCurrentTime] = useState("POMO" || "SHORT" || "BREAK");
-  const {isActive, setIsActive}=useState(false)
+  const [time, setTime] = useState(25 * 60); // 25 minutes in seconds
+  const [currentTime, setCurrentTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
+  useEffect(() => {
+    let interval = null;
 
+    if (isActive) {
+      //Run timer
+      interval = setInterval(() => {
+        setTime(time => time - 1);
+      }, 1000);
+
+    } else {
+      //clear interval
+      clearInterval(interval);
+    }
+
+    if( time === 0){
+      setIsActive(false)
+      setIsWorking((prev)=>!prev)
+      setTime(isWorking ? 300 : 1500) // 5 minutes for short break, 25 minutes for pomodoro
+
+    }
+    return () => clearInterval(interval)
+  }, [isActive, time]);
+
+  function handleStartStop() {
+    playSound();
+    setIsActive(!isActive);
+  }
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/start-13691.mp3")
+    );
+    await sound.playAsync();
+  }
 
   return (
     <SafeAreaView
@@ -19,7 +60,7 @@ export default function App() {
     >
       <View
         style={{
-          flex:1,
+          flex: 1,
           paddingHorizontal: 15,
           paddingTop: Platform.OS === "android" && 30,
         }}
@@ -31,8 +72,10 @@ export default function App() {
           setTime={setTime}
         />
         <Timer time={time} />
-        <TouchableOpacity style={styles.button}>
-          <Text style={{color:"white", fontWeight:"bold"}}>{isActive?"STOP":"START"}</Text>
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            {isActive ? "STOP" : "START"}
+          </Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -44,11 +87,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   text: { fontSize: 32, fontWeight: "bold" },
-  button:{
-    backgroundColor:"#333333",
-    padding:15,
-    marginTop:15,
-    borderRadius:15,
-    alignItems:"center"
-  }
+  button: {
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    alignItems: "center",
+  },
 });
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,27 +1,29 @@
-import { View , Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-function Timer({time}) {
-    const formatedTime = `${Math.floor(time/60).toString().padStart(2,"0")}:${(time%60).toString().padStart(2,"0")}`
+function Timer({ time }) {
+  const formatedTime = `${Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0")}:${(time % 60).toString().padStart(2, "0")}`;
   return (
-   <View style={styles.container}>
-    <Text style={styles.time}>{formatedTime}</Text>
-   </View>
-  )
+    <View style={styles.container}>
+      <Text style={styles.time}>{formatedTime}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:0.3,
-        justifyContent:"center",
-        backgroundColor: "#F2F2F2",
-        padding:15,
-        borderRadius:15,
-    },
-    time:{
-        fontSize:80,
-        fontWeight:"bold",
-        textAlign:"center",
-        color: "#333333",
-    }
-})
-export default Timer
+  container: {
+    flex: 0.3,
+    justifyContent: "center",
+    backgroundColor: "#F2F2F2",
+    padding: 15,
+    borderRadius: 15,
+  },
+  time: {
+    fontSize: 80,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#333333",
+  },
+});
+export default Timer;
